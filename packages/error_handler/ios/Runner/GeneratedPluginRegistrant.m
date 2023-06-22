23b712b62b52d07db5afab2bac1dde6f357f8eeb
erazor <52050311+erazoor@users.noreply.github.com>
2023-06-22 18:01:39 +0200

    Revert "t"
    This reverts commit d83cb4198fe66d14941092d61ed3e55f873d67e3.

---
+++ packages/error_handler/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<connectivity/FLTConnectivityPlugin.h>)
+#import <connectivity/FLTConnectivityPlugin.h>
+#else
+@import connectivity;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
+}
+
+@end
