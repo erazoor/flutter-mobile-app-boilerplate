151cbcb3b12629bfa51347ea38c9dcb6bfb272b8
erazor <pierregi31.12@gmail.com>
2023-07-10 16:31:45 +0200

    feat: Stateful Widget Example

--- lib/app/views/home_page.dart
+++ lib/app/views/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-import 'package:flutter_mobile_app_boilerplate/app/themes/fonts.dart'
-    as app_fonts;
+import 'package:flutter_mobile_app_boilerplate/app/widgets/display_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key ?key}) : super(key: key);
@@ -19,14 +17,11 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
         child: 
-          Text(
-            'Welcome to the Home Page!',
-            style: app_fonts.AppFonts.h1,
-          ),
+          DisplayCard(),
       ),
     );
   }
---
+++ lib/app/widgets/display_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_mobile_app_boilerplate/app/config/config_file.dart'
+    as config;
+import 'package:flutter_mobile_app_boilerplate/app/themes/fonts.dart'
+    as app_fonts;
+
+class DisplayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              config.appName,
+              style: app_fonts.AppFonts.h1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
