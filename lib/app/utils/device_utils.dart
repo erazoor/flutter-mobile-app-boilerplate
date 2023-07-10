4c1ef0bd4d648fb6aceafa2b27f92982dd3d72d6
erazor <pierregi31.12@gmail.com>
2023-07-10 11:29:14 +0200

    refactor: Optimized code
    - Changed methods
    - Created new methods based on old ones

--- lib/app/utils/device_utils.dart
+++ lib/app/utils/device_utils.dart
@@ -1,20 +1,54 @@
 import 'package:location/location.dart';
 import 'package:connectivity/connectivity.dart';
+import 'package:permission_handler/permission_handler.dart' as permissionHandler;
 
 class DeviceUtils {
   Future<bool> hasInternetConnection() async {
     ConnectivityResult connectivityResult = await Connectivity().checkConnectivity();
-
-    if (connectivityResult == ConnectivityResult.mobile ||
-        connectivityResult == ConnectivityResult.wifi) {
-      return true; // Internet connection is available
-    }
-
-    return false; // Internet connection is not available
+    return connectivityResult == ConnectivityResult.mobile || connectivityResult == ConnectivityResult.wifi;
   }
 
   static Future<bool> isLocationEnabled() async {
-    bool serviceEnabled = await Location().serviceEnabled();
-    return serviceEnabled;
+    return await Location().serviceEnabled();
+  }
+
+  static Future<bool> isLocationPermissionGranted() async {
+    PermissionStatus permissionGranted = await Location().hasPermission();
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isCameraPermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.camera.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isStoragePermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.storage.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isMicrophonePermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.microphone.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isContactsPermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.contacts.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isPhonePermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.phone.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isSmsPermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.sms.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> isCalendarPermissionGranted() async {
+    PermissionStatus permissionGranted = (await permissionHandler.Permission.calendar.status) as PermissionStatus;
+    return permissionGranted == PermissionStatus.granted;
   }
 }
