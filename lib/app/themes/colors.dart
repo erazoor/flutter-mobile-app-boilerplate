297999c104f168db262ecc15b0e6cf3ac45ea819
erazor <pierregi31.12@gmail.com>
2023-07-10 11:57:41 +0200

    chore: Embeded styling

--- lib/app/themes/colors.dart
+++ lib/app/themes/colors.dart
@@ -4,3 +4,8 @@ class AppColors {
   static const Color primary = Color(0xFF0D47A1);
   static const Color primaryDark = Color(0xFF002171);
 }
+
+class ErrorColors {
+  static const Color primary = Color.fromARGB(255, 255, 200, 200);
+  static const Color primaryBackgroundColor = Color.fromARGB(255, 185, 85, 100);
+}
--- lib/app/themes/fonts.dart
+++ lib/app/themes/fonts.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_mobile_app_boilerplate/app/themes/colors.dart'
+    as app_colors;
+
 class AppFonts {
   static const TextStyle h1 = TextStyle(
     fontSize: 32,
@@ -7,8 +10,11 @@ class AppFonts {
     letterSpacing: -1.5,
     fontFamily: 'Roboto, sans-serif',
   );
+}
 
+class ErrorFonts {
   static const TextStyle errorMessage = TextStyle(
+    color: app_colors.ErrorColors.primary,
     fontSize: 24,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
